dc9a1f557a5463441efe57227aff6ac9b2c3f0b0
Bernard-McGeever <Bernard.mcgeever@resonate.tech>
2021-03-17 07:24:03 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Java Variables: Mad Libs #
+
+## Table of Contents ##
+
+1. [Introduction and Aims](#Introduction-and-Aims)
+2. [Technologies](#Technologies)
+3. [Colaberation](#Colaberation)
+3. [Launch](#Launch)
+4. [Personal Reflection](#Personal-Reflection)
+
+## Introduction and Aims ##
+
+In this project, we’ll use Java to write a Mad Libs word game! Mad Libs have short stories with blank spaces that a player can fill in. The result is usually funny (or strange).
+
+### Aims ###
+
+1. Use Java variables
+
+## Technologies ##
+
+- Java
---
+++ src/uk/co/bernardmcgeever/Main.java
@@ -0,0 +1,64 @@
+package uk.co.bernardmcgeever;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name1 = "Bernard";
+        String name2 = "Daniel";
+        String adjective1 = "cloistered";
+        String adjective2 = "faint";
+        String adjective3 = "miscreant";
+        String verb1 = "associate";
+        String noun1 = "Accountability";
+        String noun2 = "Crayon";
+        String noun3 = "Housing";
+        String noun4 = "Kit";
+        String noun5 = "Percentage";
+        String noun6 = "Touch";
+        String place1 = "	Philippines";
+        int number = 13;
+
+        //The template for the story
+        String story = "This morning "
+            + name1
+            + " woke up feeling "
+            + adjective1
+            + ". 'It is going to be a "
+            + adjective2
+            + " day!' Outside, a bunch of "
+            + noun1
+            + "s were protesting to keep "
+            + noun2
+            + " in stores. They began to "
+            + verb1
+            + " to the rhythm of the "
+            + noun3
+            + ", which made all the "
+            + noun4
+            + "s very "
+            + adjective3
+            + ". Concerned, "
+            + name1
+            + " texted "
+            + name2
+            + ", who flew "
+            + name1
+            + " to "
+            + place1
+            + " and dropped "
+            + name1
+            + " in a puddle of frozen "
+            + noun5
+            + ". "
+            + name1
+            + " woke up in the year "
+            + number
+            + ", in a world where "
+            + noun6
+            + "s ruled the world.";
+
+        System.out.println(story);
+    }
+}
